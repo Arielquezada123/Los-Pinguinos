a4eed995d52e8a8140f5c83d91f21ebe274756f5
Ariel <ariel.quezada06@inacapmail.cl>
2025-09-26 15:52:25 -0300

    Margenes

--- watermilimiter/urls.py
+++ watermilimiter/urls.py
@@ -20,4 +20,5 @@ from django.views.generic.base import TemplateView
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',TemplateView.as_view(template_name="index.html"),name='index'),
+    
 ]
