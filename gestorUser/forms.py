e02731c0ce823793c604473fca5a319cc549083d
Miguel142 <miguel.castillo35@inacapmail.cl>
2025-10-03 17:18:06 -0300

    diseño de registro de cuenta

--- gestorUser/forms.py
+++ gestorUser/forms.py
@@ -3,20 +3,32 @@ from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 
 class SignUpForm(UserCreationForm):
-    first_name = forms.CharField(max_length=140, required=True)
-    last_name = forms.CharField(max_length=140, required=False)
-    email = forms.EmailField(required=True)
-    phone = forms.CharField(max_length=15, required=False)
+    Nombres = forms.CharField(max_length=140, required=True, widget=forms.TextInput(attrs={'class': 'form-control'}))
+    Apellidos = forms.CharField(max_length=140, required=False, widget=forms.TextInput(attrs={'class': 'form-control'}))
+    Correo_electrónico = forms.EmailField(required=True, widget=forms.TextInput(attrs={'class': 'form-control'}))
+    telefono = forms.CharField(max_length=15, required=False, widget=forms.TextInput(attrs={'class': 'form-control'}))
+    password1 = forms.CharField(label="Contraseña",widget=forms.PasswordInput(attrs={'class': 'form-control'}))
+    password2 = forms.CharField(label="Contraseña (confirmación)", widget=forms.PasswordInput(attrs={'class': 'form-control'}))
+
 
     class Meta:
         model = User
-        fields = ('username', 'first_name', 'last_name', 'email', 'password1', 'password2')
+        fields = ('username', 'Nombres', 'Apellidos', 'Correo_electrónico', 'password1', 'password2')
+        widgets = {
+            'username': forms.TextInput(attrs={'class': 'form-control'}),
+            'Nombres': forms.TextInput(attrs={'class': 'form-control'}),
+            'Apellidos': forms.TextInput(attrs={'class': 'form-control'}),
+            'email': forms.EmailInput(attrs={'class': 'form-control'}),
+            'password1': forms.PasswordInput(attrs={'class': 'form-control'}),
+            'password2': forms.PasswordInput(attrs={'class': 'form-control'}),
+        }
 
     def save(self, commit=True):
         user = super().save(commit=False)
-        user.first_name = self.cleaned_data.get('first_name')
-        user.last_name = self.cleaned_data.get('last_name')
+        user.Nombres = self.cleaned_data.get('Nombres')
+        user.Apellidos = self.cleaned_data.get('Apellidos')
         user.email = self.cleaned_data.get('email')
         if commit:
             user.save()
         return user
+
