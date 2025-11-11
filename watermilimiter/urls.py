43c64a582039b724d227cbff93f0169b04238221
Miguel142 <miguel.castillo35@inacapmail.cl>
2025-11-10 23:58:19 -0300

    Implementada página de límite mensual

--- watermilimiter/urls.py
+++ watermilimiter/urls.py
@@ -1,10 +1,14 @@
 from django.contrib import admin
 from django.urls import path, include
 from django.views.generic.base import TemplateView
-from gestorUser.views import *
+
+
+from gestorUser.views import signUp, postlogin, limite_pagina 
+# ----------------------------------------
+
 from sensores import views as sensores_views
 from reportes.views import reportes_pagina
-from sensores.views import configuracion_pagina
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -20,19 +24,20 @@ urlpatterns = [
     path('mapa/', sensores_views.mapa_pagina_view, name='mapa_pagina'),
     path('api/popup/<str:id_mqtt>/', sensores_views.popup_lectura_latest, name='api_popup_data'),
     path('consumo/', sensores_views.consumo_pagina_view, name='consumo_pagina'),
-    path('ingreso/', sensores_views.ingreso_pagina_view, name='ingreso_pagina'),
+    
+
     path('dashboard/reportes/', reportes_pagina, name='reportes_pagina'),
     
 
     # CRUD SENSORES
-    path('ingreso/', sensores_views.ingreso_pagina_view, name='ingreso_pagina'),
+    path('ingreso/', sensores_views.ingreso_pagina_view, name='ingreso_pagina'), # <-- Esta es la que queda
     path('sensores/', sensores_views.lista_sensores_view, name='lista_sensores'),
-    path('sensores/editar/<str:id_mqtt>/', sensores_views.editar_sensor_view, name='editar_sensor'),     
+    path('sensores/editar/<str:id_mqtt>/', sensores_views.editar_sensor_view, name='editar_sensor'),    
     path('sensores/eliminar/<str:id_mqtt>/', sensores_views.eliminar_sensor_view, name='eliminar_sensor'), 
     ########################################################################################################
     path('api/inicio_data/', sensores_views.api_inicio_data, name='api_inicio_data'),
     path('api/historial/', sensores_views.historial_consumo, name='api_historial'),
+    path('dashboard/limite/', limite_pagina, name='limite_pagina'),
     
-    path('dashboard/configuracion/', configuracion_pagina, name='configuracion_pagina'),
     path('api/historial/grafico/', sensores_views.api_historial_agregado, name='api_historial_grafico'),
 ]
