0d0f71c43e2513070e61fb79e141ea96537da220
Cochayuyo <ariel.quezada06@inacapmail.cl>
2025-12-18 17:20:36 -0300

    requirements

--- watermilimiter/settings.py
+++ watermilimiter/settings.py
@@ -40,12 +40,14 @@ AUTHENTICATION_BACKENDS = (
 )
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+
 ]
 
 ROOT_URLCONF = 'watermilimiter.urls'
